93fbee6403337bc2756bf9d7a8b3c04d922f9e1a
Diogo Arrais <d.arrais@birdcontrolgroup.com>
2020-11-26 11:02:35 +0100

    Create vue project

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # personal-page
-repository to aplly knowledge after AWS CCP and do something with vue.js 
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="navbar-screen full-screen">
+        <header class="full-screen-content">
+            <h1 class="">Diogo Arrais</h1>
+            <h2>Something about me</h2>
+            <div class="technologies">
+                <img src="/img/docker.png?$modena=personal-page" alt="Docker logo" />
+                <img src="/img/docker.png?$modena=personal-page" alt="Docker logo" />
+                <img src="/img/docker.png?$modena=personal-page" alt="Docker logo" />
+
+            </div>
+        </header>
+    </div>
+
+</template>
+
+<script>
+
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+    @import '../scss/globals.scss';
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
